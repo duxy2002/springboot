72a6b79d6f76462f181ec8d1b24d515ec8c3f557
xiao-yu.du <xiao-yu.du@hpe.com>
2016-07-19 17:40:03 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# General
+
+a sample project for spring boot
---
+++ src/main/java/com/hpe/ddc/SpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.hpe.ddc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+}
